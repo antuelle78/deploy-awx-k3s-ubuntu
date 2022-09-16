92cf913470eb27b47ba1a74c38dae06b84776cd4
Michael Nelson <m.nelson@cityscoot.eu>
2022-09-16 06:26:29 +0200

    WIP

--- roles/deploy-awx-k3s-ubuntu/files/setup_kubeconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp /etc/rancher/k3s/k3s.yaml .
-sed -i 's:localhost:hoseplak3s:;s:default:"{{ ansible_hostname }}":g' k3s.yaml
-KUBECONFIG=./k3s.yaml kubectl config view --raw > config.tmp
-mv config.tmp /root/.kube/config
